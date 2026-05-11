edcf36218578f1b794f31a4de25cc6e1bea24e9a
PolyEtilen1909 <nguyenhongtonpy2004@gmail.com>
2026-05-11 19:21:46 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   const { initAuth, isAuthenticated } = useAuthStore()
 
   useEffect(() => {
-    // initAuth()
+    initAuth()
   }, [initAuth])
 
   useEffect(() => {
