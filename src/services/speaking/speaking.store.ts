cd0cdde9a71d33e338de3ddf7f3b62927b098944
PolyEtilen1909 <nguyenhongtonpy2004@gmail.com>
2026-05-18 19:48:11 +0700

    feature/call add socket io

--- src/services/speaking/speaking.store.ts
+++ src/services/speaking/speaking.store.ts
@@ -50,7 +50,7 @@ export type SpeakingState = {
   incrementTimer: () => void
 }
 
-const WS_URL = import.meta.env.VITE_WS_URL || "http://localhost"
+const WS_URL = import.meta.env.VITE_WS_URL || "http://localhost:3000"
 
 export const useSpeakingStore = create<SpeakingState>((set, get) => ({
   socket: null,
